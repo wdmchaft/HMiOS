63d7b6768091416d0d557bd56a2e11d749bea370
Maurus Kühne <mauruskuehne@gmail.com>
2011-05-24 20:35:42 +0200

    exported meta.tsx

---
+++ HarvestMoon/Resources/Meta.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Meta" tilewidth="16" tileheight="16">
+ <image source="meta.png" width="96" height="32"/>
+ <tile id="1">
+  <properties>
+   <property name="Collidable" value="True"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Collidable" value="True"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="Collidable" value="True"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="Collidable" value="True"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="Collidable" value="True"/>
+  </properties>
+ </tile>
+</tileset>
